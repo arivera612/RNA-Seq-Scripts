c3ddb1e26bafc97f4e17c54bcb52239869074b91
arivera612 <arive019@ucr.edu>
2018-04-10 14:26:32 -0700

    Create trim_galore.sh

---
+++ trim_galore.sh
@@ -0,0 +1,35 @@
+if [ -z $1 ]
+then
+    	echo "sh trim_galore.sh fastq quality length"
+        exit -1
+fi
+
+fq1=$1
+shift
+qual=$1
+shift
+length=$1
+shift
+trim=$1
+#shift
+#out=$1
+
+SEQFILE="$fq1"
+if [ ! -z $1 ] || [[ $1 =~ '^[^-]' ]]
+then
+    	fq2=$1
+	SEQFILE="$fq1 $fq2"
+        shift
+fi
+
+echo $SEQFILE
+
+module load fastqc/0.11.3
+module load trim_galore/0.4.2
+
+out=$(dirname $fq1)
+#echo $out
+
+trim_galore --clip_R1 13 -o $out -q $qual -length $length -fastqc $fq1
+#fastqc -o $out -f fastq $SEQFILE
+
