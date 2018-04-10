554e3d0d053b830a76fb38aea6e3452c4f8f67c8
arivera612 <arive019@ucr.edu>
2018-04-10 14:30:59 -0700

    Create trim_galore_directory.sh

---
+++ trim_galore_directory.sh
@@ -0,0 +1,24 @@
+#Takes a directory as input and loops through the files
+if [ -z $1 ]
+then
+    	echo "sh trim_galore.sh directory score length"
+        exit -1
+fi
+
+dir=$1
+shift
+qual=$1
+shift
+length=$1
+shift
+
+scriptsdir="/bigdata/messaoudilab/arivera/Scripts"
+directory=$(readlink -f "$dir")
+
+for f in `$directory`*.gz
+do
+  	out=$(dirname $f)
+        echo "$f"
+        sbatch --wrap "$scriptsdir/trim_galore.sh $f $qual $length "
+
+done
